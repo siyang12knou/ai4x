1ad0f58fb9a2e4cae2ff5c4a046913ef974ddb18
siyang12knou <siyang12@knou.ac.kr>
2022-12-05 17:31:53 +0900

    case 함수 추가

--- commons/src/main/java/com/kailoslab/ai4x/commons/utils/Utils.java
+++ commons/src/main/java/com/kailoslab/ai4x/commons/utils/Utils.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,6 +12,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.io.Resource;
 
 import java.io.FileNotFoundException;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.util.*;
@@ -79,8 +82,8 @@ public class Utils {
                 ));
     }
 
-    public static String toCamelCase(String text) {
-        String[] words = text.split("[\\W_]+");
+    public static String snakeToCamel(String snake) {
+        String[] words = snake.split("[\\W_]+");
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < words.length; i++) {
             String word = words[i];
@@ -94,8 +97,45 @@ public class Utils {
         return builder.toString();
     }
 
+    public static String camelToSnake(String camel) {
+        return camel.replaceAll("([a-z])([A-Z]+)", "$1_$2").toLowerCase();
+    }
+
     public static String toFirstLowerCase(String text) {
-        return Character.toLowerCase(text.charAt(0)) + text.substring(1);
+        return Character.toLowerCase(text.charAt(0)) + text.length() > 1 ? text.substring(1) : "";
+    }
+
+    public static String toFirstUpperCase(String text) {
+        return Character.toUpperCase(text.charAt(0)) + text.length() > 1 ? text.substring(1) : "";
+    }
+
+    public static <T> T newInstance(Map<String, String> properties, Class<T> clazz) {
+        if(ObjectUtils.isEmpty(properties)) {
+            return null;
+        }
+
+        try {
+            T instance = clazz.getDeclaredConstructor().newInstance();
+            Field[] fields = clazz.getFields();
+            Arrays.stream(fields).forEach(field -> {
+                String snakeName = Utils.camelToSnake(field.getName());
+                if(StringUtils.isNotEmpty(properties.get(snakeName))) {
+                    try {
+                        Method setMethod = clazz.getMethod("set" + toFirstUpperCase(field.getName()));
+                        Class<?>[] params = setMethod.getParameterTypes();
+                        if(params.length == 1) {
+                            Method valueOf = params[0].getMethod("valueOf", String.class);
+                            setMethod.invoke(instance, valueOf.invoke(null, properties.get(snakeName)));
+                        }
+                    } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException ignored) {}
+                }
+            });
+
+            return instance;
+
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            return null;
+        }
     }
 
     public static List<String> getScanPackages(ApplicationContext applicationContext) {
